d05152a995568a4d7de2b3be96ba34d5c1f08f9f
Voxys <50014072+Voxys@users.noreply.github.com>
2023-01-23 18:39:31 -0500

    Variables description fix

--- variables.tf
+++ variables.tf
@@ -10,12 +10,12 @@ variable "location" {
 
 variable "vn_address_space" {
   type        = list(string)
-  description = "Virtual Network address space (input example: 172.16.0.0/16)"
+  description = "Virtual Network address space (input example: [\"172.16.0.0/16\"])"
 }
 
 variable "vn_subnet" {
   type        = list(string)
-  description = "Subnet used by the Azure Virtual Network (input example: 172.16.1.0/24)"
+  description = "Subnet used by the Azure Virtual Network (input example: [\"172.16.1.0/24\"])"
 }
 
 variable "azure_location" {
